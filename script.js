92a7ab56ab136f27d9313fa32b9d13b04a6cdfc6
Dijin Leo <dijinleo@gmail.com>
2025-04-05 15:56:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function clickSubmitButton(event){
+    event.preventDefault();
+    const username=document.getElementById("username").value;
+    const welcomeMessage= document.getElementById("welcome-message");
+    welcomeMessage.textContent ="Welcome, "+username+'!';
+
+    welcomeMessage.style.display="block";
+}
